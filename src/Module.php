57b2b35878bbc4df22add0163cad777b6490f046
David J Eddy <me@davidjeddy.com>
2017-05-14 20:47:57 -0400

    REMOVED empty int() method in ./src/Module

--- src/Module.php
+++ src/Module.php
@@ -2,15 +2,20 @@
 
 namespace davidjeddy\freeradius;
 
+/**
+ * Class Module
+ *
+ * @package davidjeddy\freeradius
+ */
 class Module extends \yii\base\Module
 {
+    /**
+     * @var string
+     */
     public $controllerNamespace = 'davidjeddy\freeradius\controllers';
+
+    /**
+     * @var string
+     */
     public $defaultRoute        = 'default/index';
-
-    public function init()
-    {
-        parent::init();
-
-        // custom initialization code goes here
-    }
 }
